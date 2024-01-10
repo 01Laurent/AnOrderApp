8af9298d4f4dd1dd60097b39cbe67d850e4dad6c
jane rose <rosejaneforyou@gmail.com>
2024-01-10 11:36:18 +0300

    Add method DisplayCartItems

--- AnOrderApp/Cart.cs
+++ AnOrderApp/Cart.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anorderapp
+{
+    class Cart
+    {
+        private List<string> cartItems;
+
+        public void DisplayCartItems()
+        {
+            cartItems = new List<string>();
+
+            if(cartItems != null )
+            {
+                foreach( string item in cartItems )
+                {
+                    cartItems.Add( item );
+                }
+            }
+            else
+            {
+                Console.WriteLine("Your Cart Is Empty");
+            }
+        }
+    }
+}
